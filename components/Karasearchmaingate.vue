d84ca778d1574438fa48cdcf5f7695949fec94ed
Osumi Akari <kig.osumiakari@gmail.com>
2023-07-29 17:55:21 +0900

    add searchgate

---
+++ components/Karasearchmaingate.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero is-halfheight">
+        <div class="hero-body">
+            <div class="columns">
+                <div class="column container">
+                    <h1 class="title">Karasearch</h1>
+                    <p class="subtitle">明瞭に二次利用が許可されたカラオケ音源の検索</p>
+                    <Searchgate />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <Karasearchmaingate />
+        <Karasearchmaindescription />
+    </div>
+</template>
